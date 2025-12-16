490cb6129c82ba9307a1e8c96f2391221b1c9e52
RyeinKim <ryein6896@gmail.com>
2025-12-16 15:05:29 +0900

    Initial commit

---
+++ src/main/java/kr/adapterz/edu_community/EduCommunityApplication.java
@@ -0,0 +1,13 @@
+package kr.adapterz.edu_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/adapterz/edu_community/EduCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.adapterz.edu_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
