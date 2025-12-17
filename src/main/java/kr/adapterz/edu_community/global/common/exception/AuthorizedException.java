a2751e7cc8e6c3e01ce90fc5f1f361922a598f7e
Ryein Kim (Wayne) <ryein6896@gmail.com>
2025-12-17 17:37:32 +0900

    Revert "Develop (#1)"
    This reverts commit cdb80e8eb1142a629df0f0720b387ec0a6840bd8.

--- src/main/java/kr/adapterz/edu_community/global/common/exception/AuthorizedException.java
+++
@@ -1,7 +0,0 @@
-package kr.adapterz.edu_community.global.common.exception;
-
-public class AuthorizedException extends BusinessException {
-    public AuthorizedException(String message) {
-        super(message, 401);
-    }
-}
