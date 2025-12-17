9adb01900bd4401e5dbecbd7672464ea9cba2157
Ryein Kim (Wayne) <ryein6896@gmail.com>
2025-12-17 17:37:32 +0900

    Revert "Develop (#1)"
    This reverts commit cdb80e8eb1142a629df0f0720b387ec0a6840bd8.

--- src/main/java/kr/adapterz/edu_community/domain/file/entity/FileCategory.java
+++
@@ -1,6 +0,0 @@
-package kr.adapterz.edu_community.domain.file.entity;
-
-public enum FileCategory {
-    PROFILE_IMAGE,
-    POST_ATTACHMENT
-}
--- src/main/java/kr/adapterz/edu_community/global/common/exception/DuplicateException.java
+++
@@ -1,7 +0,0 @@
-package kr.adapterz.edu_community.global.common.exception;
-
-public class DuplicateException extends BusinessException {
-    public DuplicateException(String message) {
-        super(message, 409);
-    }
-}
