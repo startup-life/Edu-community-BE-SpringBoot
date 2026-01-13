979d07fa438629fd70a459cc3d59bdc5a999c281
RyeinKim <ryein6896@gmail.com>
2026-01-13 10:54:37 +0900

    fix: 파라미터 명을 message -> code로 변경

--- src/main/java/kr/adapterz/edu_community/global/common/exception/BadRequestException.java
+++ src/main/java/kr/adapterz/edu_community/global/common/exception/BadRequestException.java
@@ -3,7 +3,7 @@ package kr.adapterz.edu_community.global.common.exception;
 import org.springframework.http.HttpStatus;
 
 public class BadRequestException extends BusinessException {
-    public BadRequestException(String message) {
-        super(message, HttpStatus.BAD_REQUEST);
+    public BadRequestException(String code) {
+        super(code, HttpStatus.BAD_REQUEST);
     }
 }
